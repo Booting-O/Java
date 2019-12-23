58eafe5795e081493a64eecb09920e3e03759f70
Booting-O <54933257+Booting-O@users.noreply.github.com>
2019-12-23 10:30:24 +0800

    示例代码初运行，登录、注册功能

---
+++ 课程设计源代码1.0/src/view/CourseSelection.java
@@ -0,0 +1,14 @@
+package view;
+
+public class CourseSelection {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			MainUI.show();
+		
+	}
+	public static void show() {
+
+	}
+
+}
